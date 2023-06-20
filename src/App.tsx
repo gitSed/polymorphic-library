afc7c39db722dc5ec43bc32f825898a1f8b2e3c1
Edgar Sandoval <isandoval.sed@gmail.com>
2023-06-19 20:01:38 -0600

    App component with examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Text } from './components';
+
 function App() {
-  return <div>Root Component</div>;
+  return (
+    <Text as="div">
+      <Text as="span">Span Component</Text>
+      <Text as="p">Paragraph Component</Text>
+      <Text as="a" href="www.google.com" color="red">
+        Unknow
+      </Text>
+    </Text>
+  );
 }
 
 export default App;
